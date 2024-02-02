0e1a26dda4a6ffd5c88894d3f095530ff5d1b16a
Erin Shiffler <erinmaeshiffler@gmail.com>
2024-02-02 13:21:10 -0700

    broken links fixed

--- mail.php
+++ mail.php
@@ -19,7 +19,7 @@
 
         // Set the recipient email address.
         // FIXME: Update this to your desired email address.
-        $recipient = "example@example.com";
+        $recipient = "erinmaeshiffler@gmail.com";
 
         // Set the email subject.
         $subject = "New contact from $name";
